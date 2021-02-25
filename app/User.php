14f83a09fa9972cb4e7abc2a7d84773218106613
alin <ungurean_alin@yahoo.com>
2021-02-26 00:55:20 +0200

    Add user model

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'two_factor_pass'
     ];
 
     /**
@@ -36,4 +36,30 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+        'email_verified_at',
+        'two_factor_expires_at',
+    ];
+
+    public function generate_two_factor_data()
+    {
+        $this->timestamps = false;
+        $this->two_factor_pass = random_int(100000,999999);
+        $this->two_factor_expires_at = now()->addSeconds(20);
+
+        $this->save();
+    }
+    
+    public function reset_two_factor_data()
+    {
+        $this->timestamps = false;
+        $this->two_factor_pass = null;
+        $this->two_factor_expires_at = null;
+
+        $this->save();
+    }
 }
