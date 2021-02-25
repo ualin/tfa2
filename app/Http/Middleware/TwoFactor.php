6a44f0fc72cf6b08bb9910e7a9d8c3c86eae73a7
alin <ungurean_alin@yahoo.com>
2021-02-26 00:55:53 +0200

    Add TwoFactor middleware

--- app/Http/Middleware/TwoFactor.php
+++ app/Http/Middleware/TwoFactor.php
@@ -15,6 +15,29 @@ class TwoFactor
      */
     public function handle($request, Closure $next)
     {
+        $user = auth()->user();
+
+        if(auth()->check() && $user->two_factor_pass)
+        {
+            // step 2 of the authentication
+            
+
+            if($user->two_factor_expires_at->lt(now()))
+            {
+                // the time for resolving step 2 has expired
+                $user->reset_two_factor_data();
+                auth()->logout();
+
+                return redirect()->route('login')->withMessage('The two factor code has expired. Please login again.');
+            }
+
+            // step 2 of the authentication must be resolved
+            if(!$request->is('otp*'))
+            {
+                return redirect()->route('otp.index');
+            }
+        }
+
         return $next($request);
     }
 }
